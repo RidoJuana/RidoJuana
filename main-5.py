254b0f0d323ffa3084e79e58a945ec770d152ea6
RidoJuana <zeexyee9@gmail.com>
2024-11-18 14:27:29 +0800

    Delete main-5.py

--- main-5.py
+++
@@ -1,8 +0,0 @@
-# input dari user
-data = input("masukan data : ")
-print("data : " , "tipe : ", type (data))
-
-# jika input adalah angka casting 
-angka = float(input("msukkan angka : "))
-print("data : ", angka, "tipe : ", type(angka))
-
